0845a8d2e74491196feb16d3ad842a0c9a0904a5
HS <pish019@naver.com>
2019-04-16 17:49:57 +0900

    신고리스트 안뜨네 ㅂㄷ

--- src/main/java/com/sp/admin/black/ReportServiceImpl.java
+++ src/main/java/com/sp/admin/black/ReportServiceImpl.java
@@ -3,27 +3,50 @@ package com.sp.admin.black;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sp.common.dao.CommonDAO;
 
 @Service("admin.black.reportService")
 public class ReportServiceImpl implements ReportService{
-
+	
+	@Autowired
+	private CommonDAO dao;
+	
 	@Override
 	public void insertReport(Report dto) throws Exception {
 		//신고하기
-		
+		try {
+			dao.insertData("black.insertReport", dto);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public int dataReportCount(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		int result=0;
+		
+		try {
+			result=dao.selectOne("black.dataReportCount", map);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public List<Report> listReport(Map<String, Object> map) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Report> list = null;
+		
+		try {
+			list=dao.selectList("black.listReport", map);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 
 	@Override
